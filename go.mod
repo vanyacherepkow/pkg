90489fb362e9a671b9f1a376ffdd234fd1827f1d
vanyacherepkow <F296I8ut>
2024-10-28 19:12:49 +0300

    add publisher to rbmq

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/vanyacherepkow/pkg
 go 1.23.2
 
 require (
+	github.com/ahmetb/go-linq/v3 v3.2.0
 	github.com/cenkalti/backoff/v4 v4.3.0
 	github.com/iancoleman/strcase v0.3.0
+	github.com/json-iterator/go v1.1.12
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/pkg/errors v0.9.1
 	github.com/rabbitmq/amqp091-go v1.10.0
+	github.com/satori/go.uuid v1.2.0
 	google.golang.org/grpc v1.67.1
 )
 
@@ -15,6 +18,8 @@ require (
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
@@ -23,4 +28,5 @@ require (
 	golang.org/x/text v0.17.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
---
+++ rabbitmq/publisher.go
@@ -0,0 +1,106 @@
+package rabbitmq
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/ahmetb/go-linq/v3"
+	"github.com/iancoleman/strcase"
+	"github.com/labstack/echo/v4"
+	amqp "github.com/rabbitmq/amqp091-go"
+	uuid "github.com/satori/go.uuid"
+
+	jsoniterator "github.com/json-iterator/go"
+)
+
+type IPublisher interface {
+	PublishMessage(msg interface{}) error
+	IsPublished(msg interface{}) bool
+}
+
+var publishedMessages []string
+
+type Publisher struct {
+	cfg  *RabbitMQConfig
+	conn *amqp.Connection
+	ctx  context.Context
+	//TODO: do log
+}
+
+func (p Publisher) PublishMessage(msg interface{}) error {
+	data, err := jsoniterator.Marshal(msg)
+
+	if err != nil {
+		fmt.Println("Error in marshaling message to publish message")
+		return err
+	}
+
+	typeName := reflect.TypeOf(msg).Elem().Name()
+	snakeTypeName := strcase.ToSnake(typeName)
+
+	//TODO: Add telemetry and headers
+
+	channel, err := p.conn.Channel()
+	if err != nil {
+		fmt.Println("Error in opening channel to consume message")
+		return err
+	}
+
+	defer channel.Close()
+
+	err = channel.ExchangeDeclare(
+		snakeTypeName,
+		p.cfg.Kind,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		fmt.Println("Erorr in declaring exchange to publish message")
+		return err
+	}
+
+	correlationId := ""
+
+	if p.ctx.Value(echo.HeaderXCorrelationID) != nil {
+		correlationId = p.ctx.Value(echo.HeaderXCorrelationID).(string)
+	}
+
+	publishingMsg := amqp.Publishing{
+		Body:          data,
+		ContentType:   "application/json",
+		DeliveryMode:  amqp.Persistent,
+		MessageId:     uuid.NewV4().String(),
+		Timestamp:     time.Now(),
+		CorrelationId: correlationId,
+	}
+
+	err = channel.Publish(snakeTypeName, snakeTypeName, false, false, publishingMsg)
+
+	if err != nil {
+		fmt.Println("Erorr in publishing message")
+		return err
+	}
+
+	publishedMessages = append(publishedMessages, snakeTypeName)
+
+	// h, err := jsoniter.Marshal(headers)
+
+	// if err
+	fmt.Println("Published message: %s", publishingMsg.Body)
+
+	return nil
+}
+
+func (p Publisher) IsPublished(msg interface{}) bool {
+	typeName := reflect.TypeOf(msg).Name()
+	snakeTypeName := strcase.ToSnake(typeName)
+	isPublished := linq.From(publishedMessages).Contains(snakeTypeName)
+
+	return isPublished
+}
